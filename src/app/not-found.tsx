ce5b58ceac77f60d9b0322206b753774d04adba2
Mac User <laser_yellow@cnc.jp>
2024-03-19 01:02:19 +0000

    Not Found ページ実装

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image';
+import { HomeButton } from '@/app/ui/buttons';
+
 export default function NotFound() {
-  return (<main><div></div></main>);
+  return (
+    <main className="h-full w-full">
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex flex-col items-center justify-self-center">
+          <Image src="/img/404.png" width={220} height={220} alt="AITA logo" />
+          <p>Not found a page you requested.</p>
+          <HomeButton href="/" className="mt-6">
+            HOME
+          </HomeButton>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/ui/buttons.tsx
+++ src/app/ui/buttons.tsx
@@ -38,3 +38,25 @@ export function PaginationButton({
     </Link>
   );
 }
+
+export function HomeButton({
+  children,
+  href,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  className?: string | null;
+}) {
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        'flex h-10 items-center rounded-lg bg-red-400 px-4 text-sm font-medium text-white transition-colors hover:bg-red-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 active:bg-red-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
